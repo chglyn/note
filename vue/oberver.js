687d069e2a3efd5eed6513c2c578ac9babd1ae0d
Hi，Yuan <hi-liuchangyuan@qq.com>
2020-02-08 13:29:33 +0800

    Update oberver.js

--- vue/oberver.js
+++ vue/oberver.js
@@ -1,3 +1,38 @@
+class Wacher{
+  constructor(vm, expr, cb) {
+    this.vm = vm;
+    this.expr = expr;
+    this.cb = cb;
+    this.oldVal = this.getOldVal();
+  }
+  getOldVal() {
+    Dep.target = this;
+    const oldVal = compileUtil.getVal(this.expr, this.vm);
+    Dep.target = null;
+    return oldVal;
+  }
+  update() {
+    const newVal = compileUtil.getVal(this.expr, this.vm);
+    if(newVal !== oldVal) {
+      this.cb(newVal);
+    }
+  }
+}
+
+
+class Dep{
+  constructor() {
+    this.subs = [];
+  }
+  addSub(watcher) {
+      this.subs.push(watcher);
+  }
+  //通知观察者更新
+  notify() {
+    this.subs.forEach(w => w.update());
+  }
+}
+
 
 class Oberver{
   constructor(data) {
@@ -14,11 +49,13 @@ class Oberver{
   
   definedReactive(data, key, value) {
     this.observer(value);
+    const dep = new Dep();
     Object.defineProperty(obj, key, {
       enumberable: true,
       configurable: false,
       get() {
         //订阅数据变化时， 往Dep中添加观察者
+        Dep.target && dep.addSub(Dep.target);
         return val;
       },
       set(newVal) => {
@@ -26,6 +63,8 @@ class Oberver{
             if(newVal == val) {
                 value = newVal;
             }
+            //通知变化
+            dep.notify();              
       }
     })
   }
