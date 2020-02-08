78918d4e9bf4cfb2f07f5ccf0f165ac6ae00c39a
Hi，Yuan <hi-liuchangyuan@qq.com>
2020-02-08 13:15:10 +0800

    Update oberver.js

--- vue/oberver.js
+++ vue/oberver.js
@@ -1,1 +1,32 @@
 
+class Oberver{
+  constructor(data) {
+    this.oberver(data);
+  }
+  
+  observer(data) {
+    if(data && typeof === 'object') { 
+        Object.keys(data).forEach(val => {
+          this.defineReactive(data, key, data[key]);
+        })
+    }
+  }
+  
+  definedReactive(data, key, value) {
+    this.observer(value);
+    Object.defineProperty(obj, key, {
+      enumberable: true,
+      configurable: false,
+      get() {
+        //订阅数据变化时， 往Dep中添加观察者
+        return val;
+      },
+      set(newVal) => {
+            this.oberver(newVal);
+            if(newVal == val) {
+                value = newVal;
+            }
+      }
+    })
+  }
+}
